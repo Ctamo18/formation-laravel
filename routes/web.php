6d30fbd82ca042e6d93f8816b60225807ef86b5c
Ctamo18 <tamochantal5@gmail.com>
2021-05-26 16:13:22 +0000

    create home view and redirect

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('test/', function () {
     return view('test');
 });
+
+Route::redirect('/here', '/test');
+
+Route::view('/home', 'home', ['name'=>'Tamo', 'last_name'=>"Chantal" ]);
